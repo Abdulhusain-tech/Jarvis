1957c07c710f0b8e4f325dd6c78697fabd2ed2a3
TechieAbdulHusain <abdulhusainratlam.ar@gmail.com>
2025-12-19 17:09:13 +0530

    voice setup done

--- engine/command.py
+++ engine/command.py
@@ -1,14 +1,38 @@
 import pyttsx3
+import speech_recognition as sr
 
 
 def speak(text):
 	engine = pyttsx3.init('sapi5')
 	voices = engine.getProperty('voices')
 	print(voices)
-	engine.setProperty('voice', voices[2].id)
-	engine.setProperty('rate', 175)
+	engine.setProperty('voice', voices[1].id)
+	engine.setProperty('rate', 125)
 
 	engine.say(text)
 	engine.runAndWait()
 
-speak("Hi Abdulhusain! How are you? I hope you are fine")
+def takecommand():
+
+	r = sr.Recognizer()
+
+	with sr.Microphone() as source:
+		print('listening....')
+		r.pause_threshold = 1
+		r.adjust_for_ambient_noise(source)
+
+		audio = r.listen(source, 10, 6)
+
+	try:
+		print('recognizing')
+		query = r.recognize_google(audio, language='en-in')
+		print(f'user said: {query}')
+	except Exception as e:
+		return ""
+	
+	return query.lower()
+
+text = takecommand()
+
+
+speak(text)
