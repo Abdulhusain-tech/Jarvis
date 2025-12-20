1e91afb239dbf527043a9cd5cb98d5690cac5de7
TechieAbdulHusain <abdulhusainratlam.ar@gmail.com>
2025-12-20 11:54:11 +0530

    cricbuzz added

--- engine/db.py
+++ engine/db.py
@@ -9,9 +9,9 @@ cursor = con.cursor()
 # query = "INSERT INTO sys_command VALUES (null,'vlc', 'C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\VideoLAN\\VLC media player.lnk')"
 # cursor.execute(query)
 # con.commit()
-query = "INSERT INTO sys_command VALUES (null,'local send', 'C:\\Users\\Abdulhusain\\Downloads\\LocalSend\\localsend_app.exe')"
-cursor.execute(query)
-con.commit()
+# query = "INSERT INTO sys_command VALUES (null,'local send', 'C:\\Users\\Abdulhusain\\Downloads\\LocalSend\\localsend_app.exe')"
+# cursor.execute(query)
+# con.commit()
 
 # query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
 # cursor.execute(query)
@@ -32,3 +32,7 @@ con.commit()
 # query = "INSERT INTO web_command VALUES (null,'youtube', 'https://www.youtube.com/')"
 # cursor.execute(query)
 # con.commit()
+
+query = "INSERT INTO web_command VALUES (null,'cricbuzz', 'https://www.cricbuzz.com/')"
+cursor.execute(query)
+con.commit()
